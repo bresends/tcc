6df79e6cab7f49de79f8313bc3ff3420c3284336
Bruno Resende dos Santos <bruno.resendesantos45@gmail.com>
2025-05-03 12:59:07 -0300

    feat: basis chat logic added

--- src/app.py
+++ src/app.py
@@ -3,34 +3,57 @@ import google.genai as genai
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-api_key = os.getenv("GOOGLE_API_KEY")
-
-if not api_key:
-    st.error("Please set the GOOGLE_API_KEY environment variable to the path of your service account key file.")
-    st.stop()
-
-client = genai.Client(api_key=api_key)
+def initialize_google_genai():
+    try:
+        load_dotenv()
+        api_key = os.getenv("GOOGLE_API_KEY")
+        if not api_key:
+            raise ValueError("API key not found in environment variables.")
+        return genai.Client(api_key=api_key)
+    except Exception as e:
+        st.error(f"Error initializing Google GenAI: {e}")
+        return None
 
 def get_gemini_response(question):
+    client = initialize_google_genai()
+    if client is None:
+        return "Error initializing Google GenAI client."
+
     response = client.models.generate_content(
         model="gemini-2.0-flash",
         contents=question,
     )
     return response.text
 
-st.set_page_config(
-    page_title="Normas Técnicas Q&A",
-    page_icon="📚"
-)
+def app():
+    # Initialize Streamlit app
+    st.set_page_config(
+        page_title="Normas Técnicas Q&A",
+        page_icon="📚",
+    )
 
-st.title="Normas Técnicas Q&A"
-st.header("Gemini Pro Chatbot")
+    st.title("📚 Normas Técnicas Q&A")
 
-input = st.text_input("Ask me anything")
-submit = st.button("Generate Response")
+    user_message = st.chat_input("Faça uma pergunta sobre as normas técnicas: ")
 
-if submit:
-    response = get_gemini_response(input)
-    st.write(response)
+    if user_message:
+        if "messages" in st.session_state:
+            messages = st.session_state["messages"]
+        else:
+            messages = []
+            st.session_state["messages"] = messages
+
+        messages.append({"user": "user", "text": user_message})
+
+        # mensagem de resposta do assistant
+        response = get_gemini_response(user_message)
+
+        messages.append({"user": "assistant", "text": response})
+
+        for message in messages:
+            # colocar a mensagem do usuário na tela
+            with st.chat_message(message["user"]):
+                st.write(message["text"])
+
+if __name__ == "__main__":
+    app()
